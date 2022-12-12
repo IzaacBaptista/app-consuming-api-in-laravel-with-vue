4f23f520da457782cfa4a0276ea195fa174796c9
Izaac Baptista <izaacbaptista@gmail.com>
2022-12-12 16:52:36 -0300

    configurando rotas protegidas por autorização

--- routes/api.php
+++ routes/api.php
@@ -19,29 +19,29 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //route cliente
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
+Route::apiResource('cliente', 'App\Http\Controllers\ClienteController')->middleware('jwt.auth');
 
 //route carro
-Route::apiResource('carro', 'App\Http\Controllers\CarroController');
+Route::apiResource('carro', 'App\Http\Controllers\CarroController')->middleware('jwt.auth');
 
 //route locação
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController')->middleware('jwt.auth');
 
 //route marca
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
+Route::apiResource('marca', 'App\Http\Controllers\MarcaController')->middleware('jwt.auth');
 
 //modelo
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
+Route::apiResource('modelo', 'App\Http\Controllers\ModeloController')->middleware('jwt.auth');
 
 //login
-Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('login', 'App\Http\Controllers\AuthController@login')->middleware('jwt.auth');
 
 //logout
-Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout')->middleware('jwt.auth');
 
 //refresh
-Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh')->middleware('jwt.auth');
 
 //me
-Route::post('me', 'App\Http\Controllers\AuthController@me');
+Route::post('me', 'App\Http\Controllers\AuthController@me')->middleware('jwt.auth');
 
