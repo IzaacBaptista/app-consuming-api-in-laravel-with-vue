6ab21481e1b07a60b648e748a39ae21b8e3770c6
Izaac Baptista <izaacbaptista@gmail.com>
2022-12-12 16:58:29 -0300

    criando um grupo de rotas protegidas com um prefix de versionamento

--- routes/api.php
+++ routes/api.php
@@ -18,30 +18,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//route cliente
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController')->middleware('jwt.auth');
+Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
+    Route::apiResource('carro', 'App\Http\Controllers\CarroController');
+    Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+    Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
+    Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
+});
 
-//route carro
-Route::apiResource('carro', 'App\Http\Controllers\CarroController')->middleware('jwt.auth');
-
-//route locação
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController')->middleware('jwt.auth');
-
-//route marca
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController')->middleware('jwt.auth');
-
-//modelo
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController')->middleware('jwt.auth');
-
-//login
-Route::post('login', 'App\Http\Controllers\AuthController@login')->middleware('jwt.auth');
-
-//logout
-Route::post('logout', 'App\Http\Controllers\AuthController@logout')->middleware('jwt.auth');
-
-//refresh
-Route::post('refresh', 'App\Http\Controllers\AuthController@refresh')->middleware('jwt.auth');
-
-//me
-Route::post('me', 'App\Http\Controllers\AuthController@me')->middleware('jwt.auth');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
 
