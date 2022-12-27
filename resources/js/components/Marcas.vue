ff21fabcbd6a24dbd42d62482cadae16e582abcb
Izaac Baptista <izaacbaptista@gmail.com>
2022-12-27 16:58:39 -0300

    feat: Configurando os alertas de sucesso e erro do cadastro de marcas

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -71,6 +71,10 @@
     </div>
 
     <modal-component id="modalMarca" titulo="Adicionar marca">
+      <template v-slot:alertas>
+        <alert-component tipo="success"></alert-component>
+        <alert-component tipo="danger"></alert-component>
+      </template>
       <template v-slot:conteudo>
         <div class="form-group">
           <input-container-component
