c0c406cbb22f09fd1d79358a474b4998adfc8c42
Izaac Baptista <izaacbaptista@gmail.com>
2022-12-27 17:32:57 -0300

    feat: Configurando os alertas de sucesso e erro do cadastro de marcas

--- resources/js/components/Alert.vue
+++ resources/js/components/Alert.vue
@@ -2,9 +2,6 @@
   <div :class="estilo" role="alert">
       {{titulo}}
       <hr>
-      <span v-if="detalhes.data.message">{{detalhes.data.message}}</span>
-      <span v-if="detalhes.data.id">{{'ID do registro: ' + detalhes.data.id}}</span>
-      <br>
       <ul v-if="detalhes.data.errors">
           <li v-for="e, key in detalhes.data.errors" :key="key">{{ e[0] }}</li>
       </ul>
