a8bfebe99bad36c5285d8e06cf9ef22d09d98711
Restyled.io <commits@restyled.io>
2023-01-07 21:49:00 +0000

    Restyled by clang-format

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,9 +14,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    item: {}
-  },
+  state : {item : {}},
 });
 
 /**
@@ -32,25 +30,19 @@ const store = new Vuex.Store({
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0],
 // files(key).default))
 
-Vue.component(
-  "example-component",
-  require("./components/ExampleComponent.vue").default
-);
+Vue.component("example-component",
+              require("./components/ExampleComponent.vue").default);
 Vue.component("login-component", require("./components/Login.vue").default);
 Vue.component("home-component", require("./components/Home.vue").default);
 Vue.component("marcas-component", require("./components/Marcas.vue").default);
-Vue.component(
-  "input-container-component",
-  require("./components/InputContainer.vue").default
-);
+Vue.component("input-container-component",
+              require("./components/InputContainer.vue").default);
 Vue.component("table-component", require("./components/Table.vue").default);
 Vue.component("card-component", require("./components/Card.vue").default);
 Vue.component("modal-component", require("./components/Modal.vue").default);
 Vue.component("alert-component", require("./components/Alert.vue").default);
-Vue.component(
-  "paginate-component",
-  require("./components/Paginate.vue").default
-);
+Vue.component("paginate-component",
+              require("./components/Paginate.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -58,7 +50,4 @@ Vue.component(
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-  el: "#app",
-  store
-});
+const app = new Vue({el : "#app", store});
